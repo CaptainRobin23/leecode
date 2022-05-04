376658755f639ce4934a80e481908de4a4dcb675
robin <813624125@qq.com>
2022-05-05 00:18:00 +0800

    leecode eg4

--- src/main/java/com/example/leecode/Code.java
+++ src/main/java/com/example/leecode/Code.java
@@ -20,9 +20,9 @@ public class Code {
 //        for (int i = 0; i < n; i++) {
 //            numStr[i] = "" + nums[i];
 //        }
-        Arrays.sort(numStr,(k1, k2)-> {
-            String ab = k1+ k2;
-            String ba = k2+k1;
+        Arrays.sort(numStr, (k1, k2) -> {
+            String ab = k1 + k2;
+            String ba = k2 + k1;
             return ba.compareTo(ab);
         });
         StringBuilder sb = new StringBuilder();
@@ -59,14 +59,55 @@ public class Code {
     public static void eg2_numbersum() {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int ans=0;
-        int max=0;
-        int onetonplus=0;
-        while(onetonplus<N){
-            if((N-onetonplus)%(max+1)==0){ans++;}
+        int ans = 0;
+        int max = 0;
+        int onetonplus = 0;
+        while (onetonplus < N) {
+            if ((N - onetonplus) % (max + 1) == 0) {
+                ans++;
+            }
             max++;
-            onetonplus+=max;
+            onetonplus += max;
         }
         System.out.println(ans);
     }
+
+    public static void eg4(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        char[] ch = str.toCharArray();
+        int sum = 0;
+        boolean flag = false; // 负数
+        StringBuilder sb = new StringBuilder();
+        for (char c : ch) {
+            if (c >= '0' && c <= '9') {  // 如果是数字
+                if (flag) {  // 如果是负号后面的数字，加到sb中
+                    sb.append(c);
+                } else {  // 正数直接求和
+                    sum += Integer.parseInt(c + "");
+                }
+            } else if ('-' == c) {  // 如果是'-'
+                if (flag) {
+                    if (!sb.toString().isEmpty()) {  // 遇到负号后，将负号后面的连续数字组成字符串
+                        sum -= Integer.parseInt(sb.toString());  // 如果sb不空
+                        sb = new StringBuilder();
+                    }
+                }
+                flag = true;
+            } else {  // 其它字符
+                flag = false;  // 用来判断负号后的字符串结束
+                if (!sb.toString().isEmpty()) {  // 队列不空，说明负号字符串到此为止，将去整体
+                    sum -= Integer.parseInt(sb.toString());
+                    sb = new StringBuilder();
+                }
+            }
+        }
+        if (flag) {
+            if (!sb.toString().isEmpty()) {
+                sum -= Integer.parseInt(sb.toString());
+            }
+        }
+        System.out.print(sum);
+    }
+
 }
