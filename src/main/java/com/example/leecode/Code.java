639ec07ec19d067a19b13ca43d2194da2eb1f42e
robin <813624125@qq.com>
2022-05-11 00:56:39 +0800

    eg9

--- src/main/java/com/example/leecode/Code.java
+++ src/main/java/com/example/leecode/Code.java
@@ -24,8 +24,7 @@ public class Code {
         } else if ((2 * s / n - n) % 2 == 0) {
             System.out.println("无解,返回：" + -1);
             return;
-        } else
-            a1 = (2 * s / n + 1 - n) / 2;
+        } else a1 = (2 * s / n + 1 - n) / 2;
 
         for (int i = 0; i < n; i++) {
             list.add(a1 + i);
@@ -129,9 +128,184 @@ public class Code {
             }
         }
         Arrays.sort(chars);
-        if (k > s.length())
-            System.out.println(map.get(chars[s.length() - 1]));
+        if (k > s.length()) System.out.println(map.get(chars[s.length() - 1]));
         System.out.println(map.get(chars[k - 1]));
+    }
+
+
+    // 二阶矩阵求最长直线距离 F M
+    public int eg6(int[][] M) {
+        if (M == null || M.length == 0 || M[0].length == 0) return 0;
+        int ans = 0;
+        int[][] horizontal = new int[M.length][M[0].length];
+        int[][] vertical = new int[M.length][M[0].length];
+        int[][] diagonal = new int[M.length][M[0].length];
+        int[][] antidiagonal = new int[M.length][M[0].length];
+        for (int i = 0; i != M.length; ++i) {
+            for (int j = 0; j != M[0].length; ++j) {
+                if (M[i][j] == 0) {
+                    horizontal[i][j] = 0;
+                    vertical[i][j] = 0;
+                    diagonal[i][j] = 0;
+                    antidiagonal[i][j] = 0;
+                } else {
+                    horizontal[i][j] = j > 0 ? horizontal[i][j - 1] + 1 : 1;
+                    vertical[i][j] = i > 0 ? vertical[i - 1][j] + 1 : 1;
+                    diagonal[i][j] = i > 0 && j > 0 ? diagonal[i - 1][j - 1] + 1 : 1;
+                    antidiagonal[i][j] = i > 0 && j < M[0].length - 1 ? antidiagonal[i - 1][j + 1] + 1 : 1;
+                    ans = Math.max(ans, horizontal[i][j]);
+                    ans = Math.max(ans, vertical[i][j]);
+                    ans = Math.max(ans, diagonal[i][j]);
+                    ans = Math.max(ans, antidiagonal[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+
+    // 字符串子序列 下标
+    public static int eg7(String source, String target) {
+        // write your code here
+        if (source == null || target == null) {
+            return -1;
+        }
+        int index1 = source.indexOf(target);
+        return index1;
+    }
+
+    // 字符串解压 4fj7h
+    public static void eg8() {
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+        while (sc.hasNext()) {
+            str = sc.nextLine();
+        }
+        char[] ch = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ch.length; i++) {
+            if (!(ch[i] >= 'a' && ch[i] <= 'z')) {
+                System.out.print("!error");
+                return;
+            }
+            int tarNum = 1;
+            char tar = ch[i];
+            while (i + 1 < ch.length && ch[i + 1] == ch[i]) {
+                tarNum++;
+                i++;
+            }
+            sb.append(tarNum);
+            sb.append(tar);
+        }
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '1') {
+                sb.deleteCharAt(i);
+                i--;
+            }
+            if (sb.charAt(i) == '2') {
+                sb.replace(i, i + 1, sb.charAt(i + 1) + "");
+                i--;
+            }
+        }
+        System.out.print(sb.toString());
+    }
+
+    // 数组合并
+    public static void eg9() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] s1 = sc.next().split(",");
+        String[] s2 = sc.next().split(",");
+        ArrayList<String> s3 = new ArrayList<String>();
+        ArrayList<String> s4 = new ArrayList<String>();
+        ArrayList<String> s5 = new ArrayList<String>();
+
+        for (int i = 0; i < s1.length; i++) {
+            s4.add(s1[i]);
+        }
+
+        for (int i = 0; i < s2.length; i++) {
+            s5.add(s2[i]);
+        }
+
+        boolean b = true;
+        while (b) {
+            if (s4.size() > 0) {
+                if (s4.size() >= n) {
+                    for (int i = 0; i < n; i++) {
+                        s3.add(s4.get(i));
+                    }
+
+                    Iterator i = s4.iterator();
+                    int count = 0;
+                    while (i.hasNext()) {
+                        if (count >= n) {
+                            break;
+                        }
+                        s4.remove(0);
+                        count++;
+                    }
+                } else {
+                    for (int i = 0; i < s4.size(); i++) {
+                        s3.add(s4.get(i));
+                    }
+
+                    Iterator i = s4.iterator();
+                    int count = 0;
+                    int siz = s4.size();
+                    while (i.hasNext()) {
+                        if (count >= siz) {
+                            break;
+                        }
+                        s4.remove(0);
+                        count++;
+                    }
+                }
+            }
+
+            if (s5.size() > 0) {
+                if (s5.size() >= n) {
+                    for (int i = 0; i < n; i++) {
+                        s3.add(s5.get(i));
+                    }
+
+                    Iterator i = s5.iterator();
+                    int count = 0;
+                    while (i.hasNext()) {
+                        if (count >= n) {
+                            break;
+                        }
+                        s5.remove(0);
+                        count++;
+                    }
+                } else {
+                    for (int i = 0; i < s5.size(); i++) {
+                        s3.add(s5.get(i));
+                    }
+
+                    Iterator i = s4.iterator();
+                    int count = 0;
+                    int siz = s5.size();
+                    while (i.hasNext()) {
+                        if (count >= siz) {
+                            break;
+                        }
+                        s5.remove(0);
+                        count++;
+                    }
+                }
+            }
+
+            if (s4.size() == 0 && s5.size() == 0) {
+                b = false;
+            }
+        }
+
+        for (int i = 0; i < s3.size(); i++) {
+            if (i == (s3.size() - 1))
+                System.out.print(s3.get(i));
+            else
+                System.out.print(s3.get(i) + " ,");
+        }
     }
 
 
